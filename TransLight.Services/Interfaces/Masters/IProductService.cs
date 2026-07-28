81e2f76d3cc8b13a8ed8d065902ded2690ad1a25
Harsh Soni <harshsoni6011@gmail.com>
2026-07-28 09:42:47 +0530

    Refactor ProductController by moving business logic to Service.

--- TransLight.Services/Interfaces/Masters/IProductService.cs
+++ TransLight.Services/Interfaces/Masters/IProductService.cs
@@ -1,9 +1,16 @@
-﻿using TransLight.DataAccess.Models;
+﻿using TransLight.DataAccess.Common;
+using TransLight.DataAccess.Filters.Masters;
+using TransLight.DataAccess.Models;
+using TransLight.DataAccess.ViewModels.Masters;
+using TransLight.Services.Common;
 
 namespace TransLight.Services.Interfaces.Masters
 {
     public interface IProductService : IBaseService<Product>
     {
         void Update(Product obj);
+        Task<PaginatedResponse<ProductVM>> GetProductAsync(ProductFilter filter);
+        Task<ProductVM> GetForEditAsync(Guid? id);
+        Task<ServiceReturn<Guid>> SaveAsync(ProductVM vm);
     }
 }
--- TransLight.Services/Masters/ProductService.cs
+++ TransLight.Services/Masters/ProductService.cs
@@ -1,6 +1,12 @@
-﻿using TransLight.DataAccess.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TransLight.DataAccess.Common;
+using TransLight.DataAccess.Data;
+using TransLight.DataAccess.Filters.Masters;
 using TransLight.DataAccess.Models;
+using TransLight.DataAccess.ViewModels.Masters;
+using TransLight.Services.Common;
 using TransLight.Services.Interfaces.Masters;
+using TransLight.Utility.Enums;
 
 namespace TransLight.Services.Masters
 {
@@ -12,6 +18,209 @@ namespace TransLight.Services.Masters
         {
             _db.Products.Update(obj);
             _db.SaveChanges();
+        }
+
+        public async Task<PaginatedResponse<ProductVM>> GetProductAsync(ProductFilter filter)
+        {
+            var query = _db.Products.AsNoTracking().Include(x => x.Category).Include(x => x.Unit).Where(x => x.Type == (int)ProductTypes.Product);
+
+            if (!string.IsNullOrWhiteSpace(filter.Name))
+                query = query.Where(x => x.Name != null && x.Name.ToLower().Contains(filter.Name.ToLower()));
+
+            if (!string.IsNullOrWhiteSpace(filter.Make))
+                query = query.Where(x => x.Make != null && x.Make.ToLower().Contains(filter.Make.ToLower()));
+
+            if (!string.IsNullOrWhiteSpace(filter.Pack))
+                query = query.Where(x => x.Pack != null && x.Pack.ToLower().Contains(filter.Pack.ToLower()));
+
+            if (!string.IsNullOrWhiteSpace(filter.Hsn))
+                query = query.Where(x => x.Hsn != null && x.Hsn.ToLower().Contains(filter.Hsn.ToLower()));
+
+            if (!string.IsNullOrWhiteSpace(filter.CategoryName))
+                query = query.Where(x => x.Category.Name != null && x.Category.Name.ToLower().Contains(filter.CategoryName.ToLower()));
+
+            if (!string.IsNullOrWhiteSpace(filter.UnitName))
+                query = query.Where(x => x.Unit.Name != null && x.Unit.Name.ToLower().Contains(filter.UnitName.ToLower()));
+
+            if (filter.Active > -1)
+                query = query.Where(x => x.Active == filter.Active);
+
+            int totalProducts = query.Count();
+
+            var items = query
+                .Skip((filter.PageNumber - 1) * filter.PageSize)
+                .Take(filter.PageSize)
+                .Select(x => new ProductVM()
+                {
+                    Id = x.Id,
+                    Type = ProductTypes.Product,
+                    Name = x.Name,
+                    Make = x.Make,
+                    Pack = x.Pack,
+                    Rate = x.Rate,
+                    Gst = x.Gst ?? 0,
+                    Hsn = x.Hsn,
+                    Msl = x.Msl,
+                    CategoryName = x.Category.Name,
+                    Unit = x.Unit == null ? null : x.Unit.Name,
+                    Active = (YesNo)x.Active
+                }).ToList();
+
+            return new PaginatedResponse<ProductVM>
+            {
+                Items = items,
+                TotalItems = totalProducts,
+                TotalPages = (int)Math.Ceiling((double)totalProducts / filter.PageSize),
+                CurrentPage = filter.PageNumber
+            };
+        }
+
+        public async Task<ProductVM> GetForEditAsync(Guid? id)
+        {
+            if (id == null)
+                return new ProductVM();
+
+            var productData = _db.Products
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Unit)
+                .Where(x => x.Id == id).FirstOrDefault();
+
+            if (productData == null)
+                return new ProductVM();
+
+            var productVM = new ProductVM()
+            {
+                Id = productData.Id,
+                Type = ProductTypes.Product,
+                Name = productData.Name,
+                Make = productData.Make,
+                Pack = productData.Pack,
+                Rate = productData.Rate,
+                Gst = productData.Gst ?? 0,
+                Hsn = productData.Hsn,
+                Msl = productData.Msl,
+                CategoryId = productData.CategoryId,
+                CategoryName = productData.Category.Name,
+                UnitId = productData.UnitId,
+                Unit = productData.Unit?.Name,
+
+                RawMaterials = _db.ProductRawMaterials
+                    .AsNoTracking()
+                    .Where(x => x.ProductId == id && x.Type == (int)ProductTypes.RawMaterial)
+                    .Select(x => new ProduceRawMaterialsVM
+                    {
+                        Id = x.Id,
+                        ProductId = x.ProductId,
+                        RawMaterialId = x.RawMaterialId,
+                        RawMaterialName = x.RawMaterial.Name,
+                        UnitId = x.UnitId,
+                        UnitName = x.Unit != null ? x.Unit.Name : "",
+                        Qty = x.Qty,
+                        Type = (ProductTypes)x.Type,
+                    })
+                    .ToList(),
+
+                PackingMaterials = _db.ProductRawMaterials
+                    .AsNoTracking()
+                    .Where(x => x.ProductId == id && x.Type == (int)ProductTypes.PackingMaterial)
+                    .Select(x => new ProduceRawMaterialsVM
+                    {
+                        Id = x.Id,
+                        ProductId = x.ProductId,
+                        RawMaterialId = x.RawMaterialId,
+                        RawMaterialName = x.RawMaterial.Name,
+                        UnitId = x.UnitId,
+                        UnitName = x.Unit != null ? x.Unit.Name : "",
+                        Qty = x.Qty,
+                        Type = (ProductTypes)x.Type,
+                    })
+                    .ToList(),
+            };
+
+            return productVM;
+        }
+
+        public async Task<ServiceReturn<Guid>> SaveAsync(ProductVM vm)
+        {
+            var product = new Product()
+            {
+                Id = vm.Id ?? Guid.Empty,
+                Type = (int)ProductTypes.Product,
+                Name = vm.Name,
+                Make = vm.Make,
+                Pack = vm.Pack,
+                Rate = vm.Rate ?? 0,
+                Gst = vm.Gst,
+                CategoryId = vm.CategoryId,
+                UnitId = vm.UnitId,
+                Hsn = vm.Hsn,
+                Msl = vm.Msl,
+                Active = (int)vm.Active
+            };
+
+            if (vm.Id == null)
+            {
+                // create
+                _db.Products.Add(product);
+            }
+            else
+            {
+                _db.Products.Update(product);
+
+                #region Add/Update RawMaterials & ProduceMaterials
+                // Remove All existing RawMaterials and PackingMaterials
+                var existingMappings = _db.ProductRawMaterials
+                    .Where(x => x.ProductId == product.Id)
+                    .ToList();
+
+                if (existingMappings.Any())
+                    _db.ProductRawMaterials.RemoveRange(existingMappings);
+
+                // Update RawMaterials
+                if (vm.RawMaterials.Count > 0)
+                {
+                    foreach (var item in vm.RawMaterials.Where(x => !x.IsSelected))
+                    {
+                        _db.ProductRawMaterials.Add(new ProductRawMaterial
+                        {
+                            ProductId = product.Id,
+                            RawMaterialId = item.RawMaterialId,
+                            UnitId = item.UnitId,
+                            Qty = item.Qty,
+                            Type = (int)ProductTypes.RawMaterial
+                        });
+                    }
+                }
+
+                // Update PackingMaterials
+                if (vm.PackingMaterials.Count > 0)
+                {
+                    foreach (var item in vm.PackingMaterials.Where(x => !x.IsSelected))
+                    {
+                        _db.ProductRawMaterials.Add(new ProductRawMaterial
+                        {
+                            ProductId = product.Id,
+                            RawMaterialId = item.RawMaterialId,
+                            UnitId = item.UnitId,
+                            Qty = item.Qty,
+                            Type = (int)ProductTypes.PackingMaterial
+                        });
+                    }
+                }
+                #endregion
+            }
+
+            await _db.SaveChangesAsync();
+
+            return new ServiceReturn<Guid>
+            {
+                Success = true,
+                Message = vm.Id == null
+                    ? "Product created successfully."
+                    : "Product updated successfully.",
+                Data = product.Id
+            };
         }
     }
 }
